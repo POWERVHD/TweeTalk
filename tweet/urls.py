b39f87f8c22f71b08b0eb4191e0d49895e61582c
POWER VHD <kshitijprasad6@gmail.com>
2025-02-26 23:46:41 +0530

    Main App Redirect Resolved

--- tweet/urls.py
+++ tweet/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
 
     path('contact/', contact_view, name='contact'),
-    
+
 
 ]
--- twitter/urls.py
+++ twitter/urls.py
@@ -20,8 +20,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
 
+from django.shortcuts import redirect
+from django.contrib import admin
+from django.urls import path, include
+
+def landing_redirect(request):
+    return redirect('tweet_list')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', landing_redirect, name='landing_page'),  
+    path('tweets/', include('tweet.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
